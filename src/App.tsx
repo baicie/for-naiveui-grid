e8407964f6c747acd4335df550f5a8bd72ca12bf
刘志伟 <2055572879@qq.com>
2024-06-06 14:31:24 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import { NForm, NFormItemGi, NGrid, NGridItem, NInput } from "naive-ui";
+import {
+  NButton,
+  NForm,
+  NFormItemGi,
+  NGrid,
+  NGridItem,
+  NInput,
+} from "naive-ui";
 import { defineComponent } from "vue";
-import Child from "./components/Com";
 
 const App = defineComponent({
   setup() {
     return () => (
-      <NForm
-        labelPlacement="left"
-        showFeedback={false}
-        style={{ marginLeft: "50px" }}
-      >
-        <NGrid cols={24} xGap={20} yGap={20}>
+      <NGrid cols={24} xGap={20} yGap={20}>
+        <NGridItem span={12}>
+          <NButton>long button</NButton>
+        </NGridItem>
+        <NForm
+          labelPlacement="left"
+          showFeedback={false}
+          style={{ marginLeft: "50px" }}
+        >
           <NFormItemGi span={12}>
             <NInput />
           </NFormItemGi>
-
-          <Child />
-        </NGrid>
-      </NForm>
+        </NForm>
+      </NGrid>
     );
   },
 });
